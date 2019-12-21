4a9d0e7ccd4ba2087ef812965ab2077403dc0e39
Kapatych <onetruestory@ya.ru>
2019-12-21 18:07:26 +0300

    add footer mobile layout

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,82 +1,30 @@
 import React from 'react';
 import './footer.scss'
-
-import facebook from '../../assets/images/icon-facebook.png';
-import google from '../../assets/images/icon-google-plus.png';
-import instagram from '../../assets/images/icon-instagram.png';
-import pinterest from '../../assets/images/icon-pinterest.png';
-import twitter from '../../assets/images/icon-twitter.png';
-import youtube from '../../assets/images/icon-youtube.png';
-import paypal from '../../assets/images/icon-paypal.png';
-import amex from '../../assets/images/icon-amex.png';
-import discover from '../../assets/images/icon-discover.png';
-import mastercard from '../../assets/images/icon-mastercard.png';
-import visa from '../../assets/images/icon-visa.png';
 import {Link} from "react-router-dom";
 
 
-const Index = () => {
+const Footer = () => {
     return (
         <footer className='footer'>
-            {/*<div className='footer__top'>
-                <div className="footer__categories">
-                    <div className="footer__title">categories</div>
-                    <div className="footer__nav">
-                        <Link to='/catalog/categories/full-face-helmets' className="footer__nav-item">Full face</Link>
-                        <Link to='/catalog/categories/modular-helmets' className="footer__nav-item">Modular</Link>
-                        <Link to='/catalog/categories/dual-sport-helmets' className="footer__nav-item">Dual Sport</Link>
-                        <Link to='/catalog/categories/snowmobile-helmets' className="footer__nav-item">Snowmobile</Link>
-                        <Link to='/catalog/categories/dirt-helmets' className="footer__nav-item">Dirt</Link>
-                        <Link to='/catalog/categories/open-face-helmets' className="footer__nav-item">Open Face</Link>
-                        <Link to='/catalog/categories/half-helmets' className="footer__nav-item">Half</Link>
-                        <Link to='/' className="footer__nav-item">intercom</Link>
-                        <Link to='/' className="footer__nav-item">pinlock (anti fog)</Link>
-                    </div>
-                </div>
-                <div className="footer__corporate-info">
-                    <div className="footer__title">Corporate Info</div>
-                    <div className="footer__nav">
-                        <Link to='/' className="footer__nav-item">About us</Link>
-                        <Link to='/' className="footer__nav-item">Careers</Link>
-                        <Link to='/' className="footer__nav-item">Privacy Policy</Link>
-                        <Link to='/' className="footer__nav-item">Terms & Conditions</Link>
-                    </div>
-                </div>
-                <div className="footer__service">
-                    <div className="footer__title">Custiomer service</div>
-                    <div className="footer__nav">
-                        <Link to='/' className="footer__nav-item">Faq</Link>
-                        <Link to='/' className="footer__nav-item">return</Link>
-                        <Link to='/' className="footer__nav-item">delivery</Link>
-                        <Link to='/' className="footer__nav-item">order tracking</Link>
-                        <Link to='/' className="footer__nav-item">contact us</Link>
-                    </div>
-                </div>
-                <div className="footer__follow-us">
-                    <div className="footer__title">Follow us</div>
-                    <div className="footer__social">
-                        <Link to='/'><img src={facebook} alt=""/></Link>
-                        <Link to='/'><img src={twitter} alt=""/></Link>
-                        <Link to='/'><img src={instagram} alt=""/></Link>
-                        <Link to='/'><img src={pinterest} alt=""/></Link>
-                        <Link to='/'><img src={youtube} alt=""/></Link>
-                        <Link to='/'><img src={google} alt=""/></Link>
-                    </div>
-                    <div className="footer__payment-methods">
-                        <Link to='/'><img src={amex} alt=""/></Link>
-                        <Link to='/'><img src={discover} alt=""/></Link>
-                        <Link to='/'><img src={visa} alt=""/></Link>
-                        <Link to='/'><img src={mastercard} alt=""/></Link>
-                        <Link to='/'><img src={paypal} alt=""/></Link>
-                    </div>
-                </div>
-            </div>*/}
-            <div className='footer__bottom'>
-                <div className="footer__logo">moto.</div>
-                <div className="footer_copyright">Copyright © 2017 MOTO. Shopify TPowered by Shopify.</div>
+            <div className="footer__social">
+                <Link to='/'><i className="fa fa-facebook-square" aria-hidden="true"/></Link>
+                <Link to='/'><i className="fa fa-twitter-square" aria-hidden="true"/></Link>
+                <Link to='/'><i className="fa fa-pinterest-square" aria-hidden="true"/></Link>
+                <Link to='/'><i className="fa fa-youtube-square" aria-hidden="true"/></Link>
+                <Link to='/'><i className="fa fa-google-plus-square" aria-hidden="true"/></Link>
+                <Link to='/'><i className="fa fa-instagram" aria-hidden="true"/></Link>
             </div>
+            <Link to='/' className="footer__logo">moto.</Link>
+            <div className="footer__payment">
+                <Link to='/'><i className="fa fa-cc-amex" aria-hidden="true"/></Link>
+                <Link to='/'><i className="fa fa-cc-discover" aria-hidden="true"/></Link>
+                <Link to='/'><i className="fa fa-cc-visa" aria-hidden="true"/></Link>
+                <Link to='/'><i className="fa fa-cc-mastercard" aria-hidden="true"/></Link>
+                <Link to='/'><i className="fa fa-cc-paypal" aria-hidden="true"/></Link>
+            </div>
+            <div className="footer__copyright">Copyright © 2017 MOTO. Shopify TPowered by Shopify.</div>
         </footer>
     )
 };
 
-export default Index;
+export default Footer;
