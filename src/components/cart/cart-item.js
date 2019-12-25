3a9ff35176383f6aaa98eddd649a8c99f992a46f
Kapatych <onetruestory@ya.ru>
2019-12-25 21:10:35 +0300

    add mobile layout for cart page

--- src/components/cart/cart-item.js
+++ src/components/cart/cart-item.js
@@ -7,29 +7,27 @@ import Counter from "../counter";
 const CartItem = ({item, addProductToCart, removeProductFromCart, deleteProduct}) => {
     const {id, thumb, title, size, color, price, quantity, total} = item;
     return (
-        <tr className='cart__item'>
-            <td>
-                <button className='cart__item-delete' onClick={() => deleteProduct(item)}>
-                    <img src={icon_x} alt=""/>
-                </button>
-            </td>
-            <td>
+        <div className='cart__item'>
+
+            <div className='cart__item-img'>
                 <img src={thumb} alt="" className='cart__item-thumb'/>
-            </td>
-            <td>
+            </div>
+
+            <div className='cart__item-info'>
                 <Link to={`catalog/${id}`} className='cart__item-title'>{title}</Link>
-                <div>
-                    <span>Size: {size} </span>
-                    <span> Color: {color}</span>
-                </div>
-            </td>
-            <td>${price}</td>
-            <td>
-                <Counter quantity={quantity} onIncrease={addProductToCart} onDecrease={removeProductFromCart} parentClass='cart' />
-                {/*<div className='cart__item-count'>{quantity}</div>*/}
-            </td>
-            <td>${total}</td>
-        </tr>
+                <div>Size: {size} </div>
+                <div> Color: {color}</div>
+                <div className='cart__item-price'>Price: ${price}</div>
+            </div>
+
+
+
+            <Counter quantity={quantity} onIncrease={addProductToCart} onDecrease={removeProductFromCart}
+                     parentClass='cart'/>
+
+            <div className='cart__item-total'>${total}</div>
+            <div className='cart__item-delete' onClick={() => deleteProduct(item)}/>
+        </div>
     )
 };
 
--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -7,36 +7,17 @@ const Cart = ({cartItems, orderTotal, addProductToCart, removeProductFromCart, d
         <div className='cart'>
             <div className='cart__wrapper'>
                 <div className="cart__title title">Shopping Cart</div>
-                <table className='cart__table table'>
-                    <thead className='table__header'>
-                    <tr>
-                        <th/>
-                        <th/>
-                        <th>Product</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Total</th>
-                    </tr>
-                    </thead>
-                    <tfoot className='table__footer'>
-                    <tr>
-                        <td/>
-                        <td className='title'>Total:</td>
-                        <td colSpan="2" className='title'>${orderTotal}</td>
-                        <td colSpan="2">
-                            <button className='cart__button button'>Proceed To Checkout</button>
-                        </td>
-                    </tr>
-                    </tfoot>
-                    <tbody className='table__content'>
                     {
                         cartItems.map((item, idx) => <CartItem key={idx} item={item}
                                                                addProductToCart={() => addProductToCart(item)}
                                                                removeProductFromCart={ () => removeProductFromCart(item)}
                                                                deleteProduct={deleteProductFromCart}/>)
                     }
-                    </tbody>
-                </table>
+                    <div className='cart__total'>
+                        <div className='title'>Total: ${orderTotal}</div>
+                        <div><button className='cart__button button'>Proceed To Checkout</button></div>
+                    </div>
+
             </div>
         </div>
     )
