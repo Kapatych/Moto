fb70e5c28f7662ddbc86fea06c3a2c30f534e5fd
Kapatych <onetruestory@ya.ru>
2020-01-10 23:01:49 +0300

    add Lazy/Suspense for container components

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import {Route, Switch} from 'react-router-dom'
 import './App.scss';
+import Spinner from "../spinner";
 
 import Header from "../../containers/header";
 import Footer from "../footer";
-import HomePage from "../../containers/home-page";
-import CartPage from "../../containers/cart-page";
-import ContactPage from "../../containers/contact-page";
-import CatalogPage from "../../containers/catalog-page";
-import ProductPage from "../../containers/product-page";
 
+const HomePage = lazy(() => import('../../containers/home-page'));
+const CartPage = lazy(() => import('../../containers/cart-page'));
+const CatalogPage = lazy(() => import('../../containers/catalog-page'));
+const ProductPage = lazy(() => import('../../containers/product-page'));
+const ContactPage = lazy(() => import('../../containers/contact-page'));
 
 function App() {
     return (
         <div className="app">
-           <Header />
+            <Header/>
             <main className='main'>
-                <Switch>
-                    <Route exact path='/' component={HomePage}/>
-                    <Route exact path='/catalog' component={CatalogPage}/>
-                    <Route exact path='/search' component={CatalogPage}/>
-                    <Route exact path='/catalog/categories/' render={ () => <div>Categories</div> }/>
-                    <Route exact path='/catalog/:productName?' component={ProductPage}/>
-                    <Route path='/catalog/categories/:categoryName?' component={CatalogPage}/>
-                    <Route path='/cart' component={CartPage}/>
-                    <Route path='/contact' component={ContactPage}/>
-                </Switch>
+                <Suspense fallback={<Spinner/>}>
+                    <Switch>
+                        <Route exact path='/' component={HomePage}/>
+                        <Route exact path='/catalog' component={CatalogPage}/>
+                        <Route exact path='/search' component={CatalogPage}/>
+                        <Route exact path='/catalog/categories/' render={() => <div>Categories</div>}/>
+                        <Route exact path='/catalog/:productName?' component={ProductPage}/>
+                        <Route path='/catalog/categories/:categoryName?' component={CatalogPage}/>
+                        <Route path='/cart' component={CartPage}/>
+                        <Route path='/contact' component={ContactPage}/>
+                    </Switch>
+                </Suspense>
             </main>
             <Footer/>
         </div>
