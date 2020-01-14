a3047ccd8c1188b1b5b2a3bf3fb53975990f5993
Kapatych <onetruestory@ya.ru>
2020-01-14 09:44:52 +0300

    Add categories for Home page

--- src/components/header/nav.js
+++ src/components/header/nav.js
@@ -4,11 +4,12 @@ import {Link} from "react-router-dom";
 import classNames from "classnames";
 import PropTypes from 'prop-types';
 
-import full_face from "../../assets/images/full_face_helmets.png";
-import modular from "../../assets/images/modular_helmets.png";
-import dual_sport from "../../assets/images/dual_sport_helmets.png";
-import dirt from "../../assets/images/dirt_helmets.png";
-
+import full_face from "../../assets/images/full_face_helmet_min.png";
+import modular from "../../assets/images/modular_helmet_min.png";
+import dual_sport from "../../assets/images/dual_sport_helmet_min.png";
+import dirt from "../../assets/images/dirt_helmet_min.png";
+import open_face from '../../assets/images/open_face_helmet_min.png';
+import half from '../../assets/images/half_helmet_min.png';
 
 const Nav = ({mobileMenu, setMobileMenu}) => {
 
@@ -37,18 +38,14 @@ const Nav = ({mobileMenu, setMobileMenu}) => {
                         <img src={dirt} alt="dirt" className='menu__link-img'/>
                         Dirt Helmets
                     </Link>
-                    {/*<Link to='/catalog/categories/snowmobile-helmets' className='menu__link'>
-                                <img src={snow} alt="snowmobile" className='menu__link-img'/>
-                                Snowmobile Helmets
-                            </Link>*/}
-                    {/*<Link to='/catalog/categories/open-face-helmets' className='menu__link'>
+                    <Link to='/catalog/categories/open-face-helmets' className='menu__link'>
                                 <img src={open_face} alt="open_face" className='menu__link-img'/>
                                 Open Face Helmets
                             </Link>
                             <Link to='/catalog/categories/half-helmets' className='menu__link'>
                                 <img src={half} alt="half" className='menu__link-img'/>
                                 Half Helmets
-                            </Link>*/}
+                            </Link>
                 </div>
             </div>
             <Link to='/contact' className='menu__link'>Contacts</Link>
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Slider from "../slider";
 import './home.scss';
 import {Link} from "react-router-dom";
-import slider_1 from '../../assets/images/slider_1.png'
-import slider_2 from '../../assets/images/slider_2.png'
-import slider_3 from '../../assets/images/slider_3.png'
+import slider_1 from '../../assets/images/slider_1.png';
+import slider_2 from '../../assets/images/slider_2.png';
+import slider_3 from '../../assets/images/slider_3.png';
+import full_face from "../../assets/images/full_face_helmet.png";
+import modular from "../../assets/images/modular_helmet.png";
+import dual_sport from "../../assets/images/dual_sport_helmet.png";
+import dirt from "../../assets/images/dirt_helmet.png";
+import half from "../../assets/images/half_helmet.png";
+import open_face from "../../assets/images/open_face_helmet.png";
 
 const Home = () => {
     return (
@@ -17,7 +23,8 @@ const Home = () => {
                         <div className='slider__item-info'>
                             <div className='slider__item-info-content'>
                                 <div className='slider__title'>O'Neal 5 Series</div>
-                                <div className='slider__subtitle'>Constructed using a lightweight polycarbonate shell</div>
+                                <div className='slider__subtitle'>Constructed using a lightweight polycarbonate shell
+                                </div>
                                 <Link to="/catalog/O'Neal-5-Series-Wingman" className='button'>view product</Link>
                             </div>
                             <div/>
@@ -50,6 +57,40 @@ const Home = () => {
 
                 </Slider>
             </div>
+
+            <div className='categories'>
+                <div className='wrapper'>
+                    <div className='categories__content'>
+                        <Link to='/catalog/categories/full-face-helmets' className='categories__item'>
+                            <img src={full_face} alt="" className='categories__item-img'/>
+                            <div className='categories__item-title title'>Full Face</div>
+                        </Link>
+                        <Link to='/catalog/categories/modular-helmets' className='categories__item'>
+                            <img src={modular} alt="" className='categories__item-img'/>
+                            <div className='categories__item-title title'>Modular</div>
+                        </Link>
+                        <Link to='/catalog/categories/dual-sport-helmets' className='categories__item'>
+                            <img src={dual_sport} alt="" className='categories__item-img'/>
+                            <div className='categories__item-title title'>Dual Sport</div>
+                        </Link>
+
+                        <Link to='/catalog/categories/dirt-helmets' className='categories__item'>
+                            <img src={dirt} alt="" className='categories__item-img'/>
+                            <div className='categories__item-title title'>Dirt</div>
+                        </Link>
+                        <Link to='/catalog/categories/half-helmets' className='categories__item'>
+                            <img src={half} alt="" className='categories__item-img'/>
+                            <div className='categories__item-title title'>Half</div>
+                        </Link>
+                        <Link to='/catalog/categories/open-face-helmets' className='categories__item'>
+                            <img src={open_face} alt="" className='categories__item-img'/>
+                            <div className='categories__item-title title'>Open Face</div>
+                        </Link>
+
+                    </div>
+                </div>
+            </div>
+
 
         </div>
     )
